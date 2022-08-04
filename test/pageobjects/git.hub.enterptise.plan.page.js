010fb5a6e571630c0c6ba40e13fe36873c339b00
pokeyrover <103198269+pokeyrover@users.noreply.github.com>
2022-08-04 13:31:27 +0300

    Delete test directory

--- test/pageobjects/git.hub.enterptise.plan.page.js
+++
@@ -1,8 +0,0 @@
-class EnterprisePlanPage {
-
-    get enterprisePlanUrl () { return 'https://github.com/organizations/enterprise_plan' }
-    get enterpriseCloudBtn () { return $('.flex-items-stretch .col-md-6:nth-child(1)') }
-    get enterpriseServerBtn () { return $('.flex-items-stretch .col-md-6:nth-child(2)') }
-}
-
-module.exports = new EnterprisePlanPage
--- test/pageobjects/git.hub.explore.page.js
+++
@@ -1,7 +0,0 @@
-class ExplorePage {
-
-    get exploreUrl () { return 'https://github.com/explore' }
-    get topicsBtn () { return $('.js-selected-navigation-item[href="/topics"]') }
-}
-
-module.exports = new ExplorePage
